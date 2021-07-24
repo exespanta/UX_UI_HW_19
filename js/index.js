a1388ad99fa9e5371b431920881a5a7fb5a155cf
Earl Espanta <78318686+exespanta@users.noreply.github.com>
2021-07-24 00:56:54 -0700

    Final

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("Your index.js file is loaded correctly!");
+
+function mouseOver() {
+    document.getElementById("navigation").style.color = "red";
+  }
+  
+  function mouseOut() {
+    document.getElementById("navigation").style.color = "black";
+  }
